3d4609093e5daa181ebfd2fad99c8b12570786ba
František Rónay <88032985+Bryndzaman78@users.noreply.github.com>
2025-03-13 18:49:10 +0100

    Pong golang/raylib

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	Wheight = 600
+	Wwidth  = 1000
+)
+
+var (
+	speed float32 = 10
+
+	rec_left = rl.NewRectangle(5, 5, 40, 100)
+
+	rec_right = rl.NewRectangle(Wwidth-45, 5, 40, 100)
+)
+
+func init() {
+	rl.InitWindow(Wwidth, Wheight, "raylib [core] example - basic window")
+	rl.SetTargetFPS(30)
+}
+
+func input() {
+	if rl.IsKeyDown(rl.KeyW) && rec_left.Y > 5 {
+		rec_left.Y -= speed
+	} else if rl.IsKeyDown(rl.KeyS) && rec_left.Y < 495 {
+		rec_left.Y += speed
+	}
+
+	if rl.IsKeyDown(rl.KeyUp) && rec_right.Y > 5 {
+		rec_right.Y -= speed
+	} else if rl.IsKeyDown(rl.KeyDown) && rec_right.Y < 495 {
+		rec_right.Y += speed
+	}
+}
+
+func draw_on_screen() {
+	rl.DrawRectangleRec(rec_left, rl.Lime)
+	rl.DrawRectangleRec(rec_right, rl.Lime)
+}
+
+func rendering() {
+	rl.BeginDrawing()
+
+	rl.ClearBackground(rl.RayWhite)
+
+	draw_on_screen()
+
+	rl.EndDrawing()
+}
+
+func quit() {
+	defer rl.CloseWindow()
+}
+
+func main() {
+	for !rl.WindowShouldClose() {
+		input()
+		rendering()
+	}
+	quit()
+}
